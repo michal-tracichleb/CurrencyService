ee4f92d9cd2b9b3bbfaa1eb25646e008ae56bd7c
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-01-28 21:21:22 +0100

    Add Model CurrencyRate

---
+++ CurrencyRates/CurrencyRates.Repository/Models/CurrencyRate.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyRates.Repository.Models
+{
+    public class CurrencyRate
+    {
+        public int Id { get; set; }
+        public string CurrencyName { get; set; }
+        public string Code { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
