395db64674ff54c83687cd189bfd8d1b3a9e0239
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-01-28 20:54:34 +0100

    Initail project ASP.NET Model-View-Controller

---
+++ CurrencyRates/CurrencyRates/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrencyRates.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
