73ec5bf53b3ef84bef2e78a9e79d681b2081a6fb
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-01-31 13:15:30 +0100

    Add CurrenciesViewModel and refactor Index action Add implement CSV generation

--- CurrencyRates/CurrencyRates/Controllers/CurrenciesController.cs
+++ CurrencyRates/CurrencyRates/Controllers/CurrenciesController.cs
@@ -1,6 +1,8 @@
-﻿using CurrencyRates.Services.Interfaces;
+﻿using CurrencyRates.Models.Currencies;
+using CurrencyRates.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace CurrencyRates.Controllers
 {
@@ -17,12 +19,35 @@ namespace CurrencyRates.Controllers
         [HttpGet]
         public async Task<IActionResult> Index(DateTime? startDate = null, DateTime? endDate = null)
         {
+            var vm = new CurrenciesViewModel();
+            vm.StartDate = startDate ?? DateTime.Today;
+            vm.EndDate = endDate ?? DateTime.Today;
+
+            var currencies = await _currencyService.GetCurrenciesByDateRangeAsync(vm.StartDate, vm.EndDate);
+            vm.Items = currencies.OrderByDescending(c => c.Date).ToList();
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadCsv(DateTime? startDate, DateTime? endDate)
+        {
             DateTime start = startDate ?? DateTime.Today;
             DateTime end = endDate ?? DateTime.Today;
 
             var currencies = await _currencyService.GetCurrenciesByDateRangeAsync(start, end);
+            var list = currencies.OrderByDescending(c => c.Date).ToList();
 
-            return View(currencies.OrderByDescending(c => c.Date).ToList());
+            var csvBuilder = new StringBuilder();
+            csvBuilder.AppendLine("CurrencyName,Code,Rate,Date,TableType");
+
+            foreach (var item in list)
+            {
+                csvBuilder.AppendLine($"{item.CurrencyName},{item.Code},{item.Rate},{item.Date:yyyy-MM-dd},{item.TableType}");
+            }
+
+            var csvBytes = Encoding.UTF8.GetBytes(csvBuilder.ToString());
+            return File(csvBytes, "text/csv", $"Currencies_{start.ToString("yyyy-MM-dd")}_{end.ToString("yyyy-MM-dd")}.csv");
         }
     }
 }
---
+++ CurrencyRates/CurrencyRates/Models/Currencies/CurrenciesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CurrencyRates.Repository.Models;
+
+namespace CurrencyRates.Models.Currencies
+{
+    public class CurrenciesViewModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<CurrencyRate> Items { get; set; }
+    }
+}
